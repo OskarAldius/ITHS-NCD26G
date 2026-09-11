b3587658b618d1745b4be374660cf078626ace8a
OskarAldius <OskarCarlNiklas.Aldius@iths.se>
2026-09-11 09:40:00 +0200

    Added method, writeline

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace simpleGit;
+﻿using System.Net.NetworkInformation;
+
+namespace simpleGit;
 
 class Program
 {
     static void Main()
     {
        System.Console.WriteLine("Hello Git!");
+       System.Console.WriteLine(DoSomething("Michael"));
+    }
+    static string DoSomething(string name)
+    {
+        
+        return $"hello{name}";
     }
 }
